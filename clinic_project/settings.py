d6a4c21bc0eb97c9cba1dd2e8bbe87933c57b062
Anas Dev <syriacano@gmail.com>
2024-04-10 05:01:20 +0200

    :add diagram img , make static conditional with debug

--- clinic_project/settings.py
+++ clinic_project/settings.py
@@ -80,15 +80,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'clinic_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 # connect database to env.py
 DATABASES = {
@@ -127,11 +118,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+else:
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
